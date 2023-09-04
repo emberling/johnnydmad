0710ba72507ed2e9e7dddbb484f980a3f7b65a4e
emberling <41180987+emberling@users.noreply.github.com>
2023-09-03 23:09:07 -0400

    make tierboss section in playlists not case sensitive
    per FF6BeyondChaos/BeyondChaosRandomizer@6ecb251

--- musicrandomizer.py
+++ musicrandomizer.py
@@ -292,7 +292,7 @@ def init_playlist(fn=DEFAULT_PLAYLIST_FILE, virtual=None, virtual_source="(virt_
     tierboss_pool = set()
     for section in playlist_parser:
         for k, v in playlist_parser[section].items():
-            if section == "tierboss":
+            if str(section).lower() == "tierboss":
                 tierboss_pool.update([s.strip() for s in v.split(',')])
             elif k in playlist_map:
                 playlist_map[k] += f", {v}"
